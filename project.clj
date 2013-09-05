6cf9d4242064cd933e049e90823615ca4905157f
Renzo Borgatti <reborg@reborg.net>
2013-09-05 17:46:09 +0100

    added trace dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/tools.trace "0.7.6"]]
   :plugins [[lein-midje "3.0.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
